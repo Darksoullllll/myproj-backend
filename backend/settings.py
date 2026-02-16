a08ed0050011d6313319b68d3988582de671f9c3
Darksoullllll <ishugautam2003@gmail.com>
2026-02-16 18:11:56 +0530

    updated backend

--- backend/settings.py
+++ backend/settings.py
@@ -139,12 +139,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
 CORS_ALLOWED_ORIGINS = [
-    "http://localhost:5174",
+    "http://localhost:5173",
+    "https://task-mang-frontend-six.vercel.app",
 
 ]
 
-import os
+CSRF_TRUSTED_ORIGINS = [
+    "https://task-mang-frontend-six.vercel.app",
+]
+
+
 
 DEBUG = os.environ.get("DEBUG", "True") == "True"
 
@@ -153,6 +159,7 @@ ALLOWED_HOSTS = ['*']  # temporarily for deployment
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MIDDLEWARE.insert(1, 'whitenoise.middleware.WhiteNoiseMiddleware')
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 CSRF_TRUSTED_ORIGINS = [
     "https://your-render-url.onrender.com",
 ]
